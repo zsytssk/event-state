61e15107abf84c2e8280d641db37b35bd1640c0f
zsytssk <zsytssk@gmail.com>
2022-03-04 17:13:10 +0800

    master:

--- event.js
+++ event.js
@@ -1,16 +1,8 @@
-"use strict";
-var __spreadArray = (this && this.__spreadArray) || function (to, from) {
-    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
-        to[j] = from[i];
-    return to;
-};
-exports.__esModule = true;
-exports.Event = void 0;
 /**
  * 事件订阅发布构造函数
  */
-var Event = /** @class */ (function () {
-    function Event(parent) {
+export class Event {
+    constructor(parent) {
         this.parent = parent;
         this.events = new Map();
         this.destroyed = false;
@@ -21,9 +13,8 @@ var Event = /** @class */ (function () {
      * @param callback
      * @param caller
      */
-    Event.prototype.on = function (event, callback, caller, once) {
-        var _this = this;
-        var events;
+    on(event, callback, caller, once) {
+        let events;
         if (this.events.has(event)) {
             events = this.events.get(event);
         }
@@ -31,85 +22,74 @@ var Event = /** @class */ (function () {
             events = new Set();
             this.events.set(event, events);
         }
-        for (var _i = 0, events_1 = events; _i < events_1.length; _i++) {
-            var temp = events_1[_i];
+        for (const temp of events) {
             if (caller === temp.caller && callback === temp.callback) {
                 return;
             }
         }
-        var off = function () {
-            _this.off(event, callback, caller);
+        const off = () => {
+            this.off(event, callback, caller);
         };
-        events.add({ caller: caller, callback: callback, once: once, off: off });
-    };
-    Event.prototype.once = function (event, callback, caller) {
+        events.add({ caller, callback, once, off });
+    }
+    once(event, callback, caller) {
         return this.on(event, callback, caller, true);
-    };
-    Event.prototype.getBind = function (event) {
+    }
+    getBind(event) {
         return this.events.get(event);
-    };
+    }
     /**
      * 取消监听，如果没有传 callback 或 caller，那么就删除所对应的所有监听
      * @param event
      * @param callback
      * @param caller
      */
-    Event.prototype.off = function (event, callback, caller) {
+    off(event, callback, caller) {
         if (!this.events.has(event)) {
             return;
         }
-        var events = this.events.get(event);
-        for (var _i = 0, _a = __spreadArray([], events); _i < _a.length; _i++) {
-            var item = _a[_i];
+        const events = this.events.get(event);
+        for (const item of [...events]) {
             if (item.callback === callback && item.caller === caller) {
-                events["delete"](item);
+                events.delete(item);
                 break;
             }
         }
-    };
-    Event.prototype.offAllCaller = function (caller) {
-        for (var _i = 0, _a = this.events.values(); _i < _a.length; _i++) {
-            var events = _a[_i];
-            for (var _b = 0, _c = __spreadArray([], events); _b < _c.length; _b++) {
-                var item = _c[_b];
+    }
+    offAllCaller(caller) {
+        for (const events of this.events.values()) {
+            for (const item of [...events]) {
                 if (item.caller === caller) {
-                    events["delete"](item);
+                    events.delete(item);
                 }
             }
         }
-    };
+    }
     /**
      * 发布消息
      * @param event
      * @param data
      */
-    Event.prototype.emit = function (event) {
+    emit(event, ...params) {
         var _a;
-        var params = [];
-        for (var _i = 1; _i < arguments.length; _i++) {
-            params[_i - 1] = arguments[_i];
-        }
-        (_a = this.parent) === null || _a === void 0 ? void 0 : _a.emit.apply(_a, __spreadArray([event], params));
+        (_a = this.parent) === null || _a === void 0 ? void 0 : _a.emit(event, ...params);
         if (!this.events.has(event)) {
             return;
         }
-        var events = this.events.get(event);
-        for (var _b = 0, _c = __spreadArray([], events); _b < _c.length; _b++) {
-            var item = _c[_b];
-            var callback = item.callback, once = item.once, off = item.off;
+        const events = this.events.get(event);
+        for (const item of [...events]) {
+            const { callback, once, off } = item;
             if (typeof callback === 'function') {
-                callback.apply(void 0, params);
+                callback(...params);
             }
             if (once) {
                 off();
             }
         }
-    };
-    Event.prototype.destroy = function () {
+    }
+    destroy() {
         this.destroyed = true;
         this.parent = undefined;
         this.events.clear();
-    };
-    return Event;
-}());
-exports.Event = Event;
+    }
+}
